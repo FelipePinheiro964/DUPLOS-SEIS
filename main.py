acb5bda7e71a902f3f8c2e4f9a730d0d75e4878f
Felipe Pinheiro <37588751-felfelipinheiro@users.noreply.replit.com>
2024-11-06 19:09:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+d1 = random.Random()
+d2 = random.Random()
+
+rolls = []
+double = 0
+for i in range(10):
+  a = d1.randrange(1, 7)
+  b = d2.randrange(1, 7)
+  rolls.append( (a,b) )
+  print(f'{a}, {b}')
+  if a + b == 12:
+    print("DUPLO SEIS")
+    double = double + 1
+
+
+
+print(rolls)
+print(double)
